e9e66aca682c56e0e068753b09dfc10dac30c3b4
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-02-07 15:23:30 +0100

    Correction de l'affichage de la page d'acceuil

--- app/Http/Controllers/MachineACafeController.php
+++ app/Http/Controllers/MachineACafeController.php
@@ -17,37 +17,6 @@ class machineACafeController extends Controller
         return view('machineACafe', ['drinks' => $drinkChoice]);
 
     }
-
-    
-    function showDrink($id) {
-        // Méthode d'appel à la BDD sans ORM
-        // $drink = DB::select("select nomboisson, prixboisson from boisson where codeboisson = ?", [$code]);
-        // Méthode d'appel à la BDD avec ORM
-        $drink = Boisson::where('code',[$id])->get();
-
-        return view('boissons.detail-boissons', ['details' => $drink]);
-        
-    }
-
-
-
-
-
-// Fonction de Thomas
-//   function listDrink()
-//   {
-//        // $drinkChoice =['Café au lait' => 60, 'Thé' => 70, 'Expresso' => 40, 'Café long' => 50];
-//       // return view('machineACafe', compact('drinkChoice'));
-
-//     $drinkChoice = DB::select('select nomBoisson, codeBoisson from boisson');
-//       return view('machineACafe', ['boissons' => $drinkChoice]);
-//   }
-
-//   function showDrink($code)
-//   {
-//     $drink = DB::select('select nomBoisson, prix from boisson where codeBoisson = ?',[$code]);
-//     return view('boisson.affichageBoisson',['detailBoisson' => $drink]);
-//   }
 }
 
 ?>
