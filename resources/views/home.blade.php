7e57cbe843d50cf4736d5dc1e43444190ca38156
Veronique Rouault <veronique.rouault@Digital-Grenoble.local>
2018-02-13 14:44:28 +0100

    Modification de l'authentification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">BACK OFFICE</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -14,10 +14,14 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Bienvenue dans la gestion de votre machine à café !
+                    
+                    <a  href="machineACafe"><img class="imageCafe" alt="image tasse a café"  src="../img/imgcafe.jpg" /></a>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+
